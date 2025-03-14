ed09422f75e672280ca2159246ef3c78749c5d34
TsitoUw <tsitohaina.pro@gmail.com>
2025-03-14 10:29:28 +0300

    feat: add database seeding script and improve user creation feedback

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function print(...args: any[]) {
+  process.stdout.write(`${args.join(" ")} \r`); // \r moves the cursor to the beginning of the line
+}
+
 const prisma = new PrismaClient();
 async function main() {
-  const arr = Array.from({ length: 50 }).map((_, i) => i);
+  const USER_LENGTH = 50;
+
+  const arr = Array.from({ length: USER_LENGTH }).map((_, i) => i);
   console.log("⚒️ Seeding the db");
 
-  for (const _ of arr) {
+  for (const i of arr) {
+    print(`${i + 1}/${USER_LENGTH}`);
     const user = await prisma.user.create({
       data: {
         id: faker.string.uuid(),
-        name: faker.name.fullName(),
+        name: faker.person.fullName(),
         email: faker.internet.email(),
         emailVerified: true,
         createdAt: new Date(),
@@ -22,7 +30,7 @@ async function main() {
     const articleArr = Array.from({
       length: faker.number.int({ min: 5, max: 20 }),
     });
-
+    print(`Creating ${articleArr.length} articles`);
     for (const _ of articleArr) {
       await prisma.article.create({
         data: {
